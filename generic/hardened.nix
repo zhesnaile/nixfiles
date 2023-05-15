516eb32ea9905d9c9c95fde5ac362025e8a60f35
zhesnaile <carlosojar@gmail.com>
2023-05-15 12:45:05 +0200

    [ADD] enabled clamav

--- generic/hardened.nix
+++ generic/hardened.nix
@@ -11,8 +11,8 @@ with lib;
 
   # enable antivirus clamav and
   # keep the signatures' database updated
-  # services.clamav.daemon.enable = true;
-  # services.clamav.updater.enable = true;
+  services.clamav.daemon.enable = true;
+  services.clamav.updater.enable = true;
 
 }
 
