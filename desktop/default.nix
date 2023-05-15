34c1a352dfcf90564ff1222ccf0fb455dc168ceb
zhesnaile <carlosojar@gmail.com>
2023-05-15 12:41:20 +0200

    [FIX] ulimits

--- desktop/default.nix
+++ desktop/default.nix
@@ -12,7 +12,8 @@
 
   security.pam.loginLimits = [
     {domain = "*";type = "-";item = "memlock";value = "infinity";}
-    {domain = "*";type = "-";item = "nofile";value = "65536";}
+    {domain = "*";type = "soft";item = "nofile";value = "65536";}
+    {domain = "*";type = "hard";item = "nofile";value = "1048576";}
   ];
 
   specialisation.vfio.configuration = {
