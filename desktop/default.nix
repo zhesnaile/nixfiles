86041db2aa0e6348f96f99f93d7bfe1e59c1f92d
zhesnaile <carlosojar@gmail.com>
2024-01-07 19:50:51 +0100

    [DEL] removed VFIO config

--- desktop/default.nix
+++ desktop/default.nix
@@ -16,17 +16,17 @@
     {domain = "*";type = "hard";item = "nofile";value = "1048576";}
   ];
 
-  specialisation.vfio.configuration = {
-    system.nixos.tags = [ "with-vfio" ];
-    mystuff.vfio = {
-      enable = true;
-      iommu-mode = "amd_iommu";
-      pci-devs = [
-        "1002:731f"
-        "1002:ab38"
-      ];
-      vendor-reset = true;
-    };
-  };
+  #specialisation.vfio.configuration = {
+  #  system.nixos.tags = [ "with-vfio" ];
+  #  mystuff.vfio = {
+  #    enable = true;
+  #    iommu-mode = "amd_iommu";
+  #    pci-devs = [
+  #      "1002:731f"
+  #      "1002:ab38"
+  #    ];
+  #    vendor-reset = true;
+  #  };
+  #};
 
 }
