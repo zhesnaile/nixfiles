69604e968d0bc91d882a124d0f04c0df935574da
zhesnaile <carlosojar@gmail.com>
2024-08-19 14:22:24 +0200

    [DEL] commented out tuxedo-keyboard module that caused build to fail

--- hosts/fattop/boot.nix
+++ hosts/fattop/boot.nix
@@ -15,15 +15,15 @@
   boot.kernelModules = [ "kvm-intel" ];
 
   # Kernel Modules
-  boot.extraModulePackages = with config.boot.kernelPackages;
-    [ tuxedo-keyboard ];
-  boot.kernelParams = [
-    "tuxedo_keyboard.mode=0"
-    "tuxedo_keyboard.color_left=0xEDEE0E"
-    "tuxedo_keyboard.color_center=0xEDEE0E"
-    "tuxedo_keyboard.color_right=0xEEEE0E"
-    "tuxedo_keyboard.brightness=25"
-    "tuxedo_keyboard.state=0"
-    "quiet"
-  ];
+  # boot.extraModulePackages = with config.boot.kernelPackages;
+  #   [ tuxedo-keyboard ];
+  # boot.kernelParams = [
+  #   "tuxedo_keyboard.mode=0"
+  #   "tuxedo_keyboard.color_left=0xEDEE0E"
+  #   "tuxedo_keyboard.color_center=0xEDEE0E"
+  #   "tuxedo_keyboard.color_right=0xEEEE0E"
+  #   "tuxedo_keyboard.brightness=25"
+  #   "tuxedo_keyboard.state=0"
+  #   "quiet"
+  # ];
 }
