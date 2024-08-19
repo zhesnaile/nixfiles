ea96f60b7651cb94b915f2ce25a95933e4d3753e
zhesnaile <carlosojar@gmail.com>
2024-08-19 14:22:48 +0200

    [ADD] enabled qtile WM

--- modules/nixos/desktop/services.nix
+++ modules/nixos/desktop/services.nix
@@ -21,6 +21,7 @@ in {
     };
 
     services.desktopManager.plasma6.enable = true;
+    services.xserver.windowManager.qtile.enable = true;
     services.displayManager.sddm.enable = true;
     services.displayManager.defaultSession = "plasma";
 
