5ba8e4b8ec202e50549f3e84b92b07aff5d3e7b5
zhesnaile <carlosojar@gmail.com>
2026-04-26 00:17:37 +0200

    feat(pkgs): added openvpn module for network manager

--- modules/nixos/base/services.nix
+++ modules/nixos/base/services.nix
@@ -3,6 +3,9 @@
 {
   # Enable network manager
   networking.networkmanager.enable = true;
+  networking.networkmanager.plugins = with pkgs; [
+    networkmanager-openvpn
+  ];
   # Enable fwupd
   services.fwupd.enable = true;
 }
